672d4d27646f3fcd5324389b27d2bb9eed0d34a9
vitaliy.kharlan.1977@gmail.com <vitaliy.kharlan.1977.gmail.com>
2025-05-30 00:28:43 +0300

    add app_colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color dateProgressIndicator = Color(0xFF9E9E9E);
-
-  static const Color description = Color(0x1F000000);
+  static const Color darkModeGreenBlack = Color(0xFF031314);
+  static const Color backgroundDarkModeAndLetters = Color(0xFF052224);
+  static const Color lettersAndIcons = Color(0xFF093030);
+  static const Color darkModeGreenBar = Color(0xFF0E3E3E);
+  static const Color mainGreen = Color(0xFF00D09E);
+  static const Color lightGreen = Color(0xFFDFF7E2);
+  static const Color backgroundGreenWhiteAndLetters = Color(0xFFF1FFF3);
+  static const Color lightBlueButton = Color(0xFF6DB6FE);
+  static const Color blueButton = Color(0xFF3299FF);
+  static const Color oceanBlueButton = Color(0xFF0068FF);
 }
